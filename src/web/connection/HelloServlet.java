004371bfc2b52996b5d88f7337ec787693c54d1d
Hwang <Hwang@windows10.microdone.cn>
2019-07-04 13:54:58 +0800

    加入web功能

---
+++ src/web/connection/HelloServlet.java
@@ -0,0 +1,61 @@
+package web.connection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Servlet implementation class hello
+ */
+@WebServlet(description = "test", urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setHeader("content-type","text/html;charset=UTF-8");
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		/* 星号表示所有的异域请求都可以接受， */
+		response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+		PrintWriter out=response.getWriter();
+		// 读取请求内容
+		BufferedReader br = new BufferedReader(new InputStreamReader(request.getInputStream(),"utf-8"));
+		String line = null;
+		StringBuilder sb = new StringBuilder();
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		//将json字符串转换为json对象
+		JSONObject json=JSONObject.parseObject(sb.toString());
+		//System.out.println(json.toString());
+		out.write("res");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
